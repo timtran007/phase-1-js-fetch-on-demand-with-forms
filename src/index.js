04ab7031954307683cbfdace8886a9c84dce346f
Tim <timtran007@gmail.com>
2022-10-02 23:24:45 -0500

    commit tree e6d131f87eb353a6f24efc848426d859cd2d2d9b

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
-  
+    let form = document.querySelector('form')
+    form.addEventListener('submit', event => {
+        event.preventDefault()
+        let input = document.querySelector('input#searchByID')
+        console.log(input.value)
+    
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then(res => res.json())
+        .then(data => {
+            let title = document.querySelector('#movieDetails h4')
+            let summary = document.querySelector('#movieDetails p')
+            title.textContent = data.title;
+            summary.textContent = data.summary;
+            console.log(`title: ${data.title}; summery: ${data.summary}`)
+        })
+    })
+    
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
